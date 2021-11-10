8ddbd7dfdd08fb1ef35f79f4069bd36c93f8df67
Mark Wainwright <wainwrightml@gmail.com>
2021-11-10 07:39:52 +0000

    POST steps now return Entity

--- Rest.Tests/IntegrationTests.cs
+++ Rest.Tests/IntegrationTests.cs
@@ -1,11 +1,15 @@
 ﻿using System.Collections.Generic;
 using System.IO.Abstractions;
 using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using Divergic.Logging.Xunit;
 using FluentAssertions;
 using Reductech.EDR.ConnectorManagement.Base;
 using Reductech.EDR.Core;
 using Reductech.EDR.Core.Abstractions;
 using Reductech.EDR.Core.Internal;
+using Reductech.EDR.Core.Internal.Serialization;
 using Reductech.EDR.Core.TestHarness;
 using Xunit;
 
@@ -15,7 +19,7 @@ namespace Reductech.EDR.Connectors.Rest.Tests
 [AutoTheory.UseTestOutputHelper]
 public partial class IntegrationTests
 {
-    public const string Skip = "manual";
+    public const string Skip = "skip";
 
     [Fact(Skip = Skip)]
     public void TestGeneratingFromFile()
@@ -29,7 +33,7 @@ public partial class IntegrationTests
                         "http://test.com",
                         null,
                         null,
-                        @"C:\Users\wainw\source\repos\Reductech\rest\Rest.Tests\Resources\Reveal.json"
+                        @"C:\Users\wainw\source\repos\Reductech\rest\Rest.Tests\Resources\RevealJson.txt"
                     )
                 )
             }
@@ -61,6 +65,59 @@ public partial class IntegrationTests
 
         result.ShouldBeSuccessful();
         result.Value.Dictionary.Keys.Should().Contain("Reveal_Cases_Get");
+    }
+
+    [Fact(Skip = Skip)]
+    public async Task TestRevealSequence()
+    {
+        var dictionary = new Dictionary<string, object>()
+        {
+            {
+                DynamicStepGenerator.SpecificationsKey, EntityConversionHelpers.ConvertToEntity(
+                    new OpenAPISpecification(
+                        "Reveal",
+                        "http://test.com",
+                        null,
+                        "https://salient-eu.revealdata.com/rest/swagger/docs/V2",
+                        null
+                    )
+                )
+            }
+        };
+
+        var assembly = Assembly.GetAssembly(typeof(RESTDynamicStep<>));
+
+        var stepFactoryResult =
+            StepFactoryStore.TryCreate(
+                ExternalContext.Default,
+                new ConnectorData(
+                    new ConnectorSettings()
+                    {
+                        Enable   = true,
+                        Id       = "Reductech.EDR.Connectors.Rest",
+                        Settings = dictionary,
+                        Version  = "1.0"
+                    },
+                    assembly
+                )
+            );
+
+        stepFactoryResult.ShouldBeSuccessful();
+        stepFactoryResult.Value.Dictionary.Keys.Should().Contain("Reveal_Cases_Get");
+
+        var runner = new SCLRunner(
+            new TestOutputLogger("Test", TestOutputHelper),
+            stepFactoryResult.Value,
+            ExternalContext.Default
+        );
+
+        var result = await runner.RunSequenceFromTextAsync(
+            "- log (EntityFormat (Reveal_WorkFolder_Create caseId: '159' InControlAuthToken: '456' userId: '789'))",
+            new Dictionary<string, object>(),
+            CancellationToken.None
+        );
+
+        result.ShouldBeSuccessful();
     }
 
     [Fact(Skip = Skip)]
